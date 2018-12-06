8a7ec5a9aa174913041b9656f21d608428866f31
徐晓东 <donald2007@163.com>
2018-12-06 17:53:40 +0800

    添加俩泛型类
    Signed-off-by: 徐晓东 <donald2007@163.com>

---
+++ src/main/java/com/kuding/dao/AbstractDaoWithClass.java
@@ -0,0 +1,58 @@
+package com.kuding.dao;
+
+import java.io.Serializable;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.Session;
+
+public abstract class AbstractDaoWithClass<T> {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	protected EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	protected abstract Class<T> clazz();
+
+	public void create(T entity) {
+		entityManager.persist(entity);
+	}
+
+	public void detach(T entity) {
+		entityManager.detach(entity);
+	}
+
+	public void createOrUpdate(T entity) {
+		entityManager.unwrap(Session.class).saveOrUpdate(entity);
+	}
+
+	public void merge(T entity) {
+		entityManager.merge(entity);
+	}
+
+	public void delete(Serializable id) {
+		T entity = entityManager.find(clazz(), id);
+		entityManager.remove(entity);
+	}
+
+	public void delete(T value) {
+		entityManager.remove(value);
+	}
+
+	public T get(Serializable id) {
+		T entity = entityManager.find(clazz(), id);
+		return entity;
+	}
+
+	public void flush() {
+		entityManager.flush();
+	}
+
+	public void refresh(T entity) {
+		entityManager.refresh(entity);
+	}
+}
---
+++ src/main/java/com/kuding/dao/BaseDaoWithClass.java
@@ -0,0 +1,476 @@
+package com.kuding.dao;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.CriteriaUpdate;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Selection;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.kuding.enums.OrderEnum;
+import com.kuding.exceptions.JpaAmebaException;
+import com.kuding.models.Page;
+import com.kuding.models.Pageable;
+import com.kuding.sqlfilter.CommonFilter;
+import com.kuding.sqlfilter.ComparebleFilterElement;
+import com.kuding.sqlfilter.FilterElement;
+import com.kuding.sqlfilter.GroupingElement;
+import com.kuding.sqlfilter.JoinTable;
+import com.kuding.sqlfilter.OrderBy;
+import com.kuding.sqlfilter.PathElement;
+
+public abstract class BaseDaoWithClass<T> extends AbstractDaoWithClass<T> {
+
+	private final Log logger = LogFactory.getLog(getClass());
+
+	/**
+	 * 获取单个实例
+	 * 
+	 * @param clazz
+	 * @param commonFilter
+	 * @return
+	 */
+	public T getSingle(CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<T> query = builder.createQuery(clazz());
+		Root<T> root = query.from(clazz());
+		if (commonFilter.getJoinList().size() > 0)
+			joinTable(commonFilter.getJoinList(), root);
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		query = predicates.length > 0 ? query.where(builder.and(predicates)) : query;
+		try {
+			T result = getEntityManager().createQuery(query).getSingleResult();
+			return result;
+		} catch (NoResultException e) {
+			logger.warn("无对象返回：" + clazz().getName());
+		}
+		return null;
+	}
+
+	/**
+	 * 获取单个实例
+	 * 
+	 * @param clazz
+	 * @param commonFilter
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public <K> K getSingle(Class<K> tarClazz, CommonFilter commonFilter) {
+		if (commonFilter.getSelectors().size() == 1) {
+			Object result = getSingleWithSingleExistJavaObj(commonFilter);
+			return (K) result;
+		}
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		if (commonFilter.getSelectors().size() == 0)
+			throw new JpaAmebaException("没有选择的字段");
+		CriteriaQuery<K> query = builder.createQuery(tarClazz);
+		Root<T> root = query.from(clazz());
+		if (commonFilter.getJoinList().size() > 0)
+			joinTable(commonFilter.getJoinList(), root);
+		List<Selection<?>> selectionList = commonFilter.getSelectors().stream().map(x -> createSelection(root, x))
+				.collect(toList());
+		query.multiselect(selectionList);
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		query = predicates.length > 0 ? query.where(builder.and(predicates)) : query;
+		try {
+			K result = getEntityManager().createQuery(query).getSingleResult();
+			return result;
+		} catch (NoResultException e) {
+			logger.warn("无对象返回：" + clazz().getName());
+		}
+		return null;
+	}
+
+	private Object getSingleWithSingleExistJavaObj(CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Object> query = builder.createQuery();
+		Root<T> root = query.from(clazz());
+		if (commonFilter.getJoinList().size() > 0)
+			joinTable(commonFilter.getJoinList(), root);
+		List<Selection<?>> selectionList = commonFilter.getSelectors().stream().map(x -> createSelection(root, x))
+				.collect(toList());
+		query.multiselect(selectionList);
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		query = predicates.length > 0 ? query.where(builder.and(predicates)) : query;
+		try {
+			Object result = getEntityManager().createQuery(query).getSingleResult();
+			return result;
+		} catch (NoResultException e) {
+			logger.warn("无对象返回：" + commonFilter.getSelectors() + "；对象：" + clazz());
+		}
+		return null;
+	}
+
+	public <K> List<K> getList(Class<K> tarClazz, CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<K> query = builder.createQuery(tarClazz);
+		Root<T> root = query.from(clazz());
+		if (commonFilter.getJoinList().size() > 0)
+			joinTable(commonFilter.getJoinList(), root);
+		List<Selection<?>> list = commonFilter.getSelectors().stream().map(x -> createSelection(root, x))
+				.collect(toList());
+		query.multiselect(list);
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		List<Order> orderList = commonFilter.getOrderList().stream()
+				.map(x -> createOrder(x.getField(), x.getValue(), builder, root)).filter(x -> x != null)
+				.collect(toList());
+		query = orderList.size() > 0 ? query.orderBy(orderList) : query;
+		query = predicates.length > 0 ? query.where(builder.and(predicates)) : query;
+		List<K> re = getEntityManager().createQuery(query).getResultList();
+		return re;
+	}
+
+	/**
+	 * 分页所做查询列表
+	 *
+	 * @param page
+	 * @param nullFilter
+	 * @param eqFilters
+	 * @return
+	 */
+	public List<T> getList(Pageable page, CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<T> query = builder.createQuery(clazz());
+		Root<T> root = query.from(clazz());
+		if (commonFilter.getJoinList().size() > 0)
+			joinTable(commonFilter.getJoinList(), root);
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		query = predicates.length > 0 ? query.where(builder.and(predicates)) : query;
+		List<Order> orderList = commonFilter.getOrderList().stream()
+				.map(x -> createOrder(x.getField(), x.getValue(), builder, root)).collect(toList());
+		orderList.add(createOrder(page.getOrderStr(), page.getOrder(), builder, root));
+		orderList = orderList.stream().filter(x -> x != null).collect(toList());
+		query = orderList.size() > 0 ? query.orderBy(orderList) : query;
+		int first = page.getEachPageSize() * (page.getPageNo() - 1);
+		List<T> list = getEntityManager().createQuery(query).setFirstResult(first).setMaxResults(page.getEachPageSize())
+				.getResultList();
+		return list;
+	}
+
+	/**
+	 * @param tarClazz
+	 * @param rootClazz
+	 * @param page
+	 * @param commonFilter
+	 * @return
+	 */
+	public <R> List<R> getList(Class<R> tarClazz, Pageable page, CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<R> query = builder.createQuery(tarClazz);
+		Root<T> root = query.from(clazz());
+		if (commonFilter.getJoinList().size() > 0)
+			joinTable(commonFilter.getJoinList(), root);
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		query = predicates.length > 0 ? query.where(builder.and(predicates)) : query;
+		List<Order> orderList = createOrderList(commonFilter.getOrderList(), page, builder, root);
+		query = orderList.size() > 0 ? query.orderBy(orderList) : query;
+		List<Selection<?>> selections = commonFilter.getSelectors().stream().map(x -> createSelection(root, x))
+				.collect(toList());
+		query.multiselect(selections);
+		int first = page.getEachPageSize() * (page.getPageNo() - 1);
+		List<R> list = getEntityManager().createQuery(query).setFirstResult(first).setMaxResults(page.getEachPageSize())
+				.getResultList();
+		return list;
+	}
+
+	public Page<T> getPage(Pageable pageable, CommonFilter filter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<T> query = builder.createQuery(clazz());
+		Root<T> root = query.from(clazz());
+		joinTable(filter.getJoinList(), root);
+		Predicate[] predicates = seperate(filter, builder, root);
+		query = predicates.length > 0 ? query.where(builder.and(predicates)) : query;
+		List<Order> orderList = createOrderList(filter.getOrderList(), pageable, builder, root);
+		query = orderList.size() > 0 ? query.orderBy(orderList) : query;
+		Page<T> page = new Page<>(pageable);
+		int first = pageable.getEachPageSize() * (pageable.getPageNo() - 1);
+		List<T> list = getEntityManager().createQuery(query).setFirstResult(first)
+				.setMaxResults(pageable.getEachPageSize()).getResultList();
+		page.setContent(list);
+		CriteriaQuery<Long> countQuery = builder.createQuery(Long.class);
+		Root<T> countRoot = countQuery.from(clazz());
+		countQuery.select(builder.count(countRoot)).where(builder.and(predicates));
+		Long count = getEntityManager().createQuery(countQuery).getSingleResult();
+		pageable.setTotalCount(count);
+		pageable.setPageCount((long) Math.ceil(pageable.getTotalCount().doubleValue() / pageable.getEachPageSize()));
+		return page;
+	}
+
+	public <R> Page<R> getPage(Class<R> tarClazz, Pageable pageable, CommonFilter filter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<R> query = builder.createQuery(tarClazz);
+		Root<T> root = query.from(clazz());
+		List<Selection<?>> selections = createSelectionList(root, filter.getSelectors());
+		query.multiselect(selections);
+		joinTable(filter.getJoinList(), root);
+		Predicate[] predicates = seperate(filter, builder, root);
+		query = predicates.length > 0 ? query.where(builder.and(predicates)) : query;
+		List<Order> orders = createOrderList(filter.getOrderList(), pageable, builder, root);
+		query = orders.size() > 0 ? query.orderBy(orders) : query;
+		int first = pageable.getEachPageSize() * (pageable.getPageNo() - 1);
+		List<R> list = getEntityManager().createQuery(query).setFirstResult(first)
+				.setMaxResults(pageable.getEachPageSize()).getResultList();
+		Page<R> page = new Page<>(pageable);
+		page.setContent(list);
+		confirmPageCount(page, builder, predicates);
+		return page;
+
+	}
+
+	private void confirmPageCount(Page<?> page, CriteriaBuilder builder, Predicate[] predicates) {
+		Pageable pageable = page.getPageable();
+		CriteriaQuery<Long> countQuery = builder.createQuery(Long.class);
+		Root<?> countRoot = countQuery.from(clazz());
+		countQuery.select(builder.count(countRoot)).where(builder.and(predicates));
+		Long count = getEntityManager().createQuery(countQuery).getSingleResult();
+		pageable.setTotalCount(count);
+		pageable.setPageCount((long) Math.ceil(pageable.getTotalCount().doubleValue() / pageable.getEachPageSize()));
+	}
+
+	/**
+	 * 获取所有数据
+	 * 
+	 * @param clazz
+	 * @return
+	 */
+	public List<T> getList() {
+		return getList(clazz(), null);
+	}
+
+	/**
+	 * 获取列表
+	 * 
+	 * @param nullFilter
+	 * @param eqFilters
+	 * @return
+	 */
+	public List<T> getList(CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<T> query = builder.createQuery(clazz());
+		Root<T> root = query.from(clazz());
+		if (commonFilter != null) {
+			if (commonFilter.getJoinList().size() > 0)
+				joinTable(commonFilter.getJoinList(), root);
+			Predicate[] predicates = seperate(commonFilter, builder, root);
+
+			query = predicates.length > 0 ? query.where(builder.and(predicates)) : query;
+			if (commonFilter.getGroupingBy().size() > 0)
+				query.groupBy(groupBy(commonFilter, builder, root));
+			List<Order> orderList = commonFilter.getOrderList().stream()
+					.map(x -> createOrder(x.getField(), x.getValue(), builder, root)).collect(toList());
+			query = orderList.size() > 0 ? query.orderBy(orderList) : query;
+		}
+		return getEntityManager().createQuery(query).getResultList();
+	}
+
+	/**
+	 * 数量
+	 *
+	 * @param nullFilter
+	 * @param eqFilters
+	 * @return
+	 */
+	public Long count(CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Long> query = builder.createQuery(Long.class);
+		Root<T> root = query.from(clazz());
+		if (commonFilter.getJoinList().size() > 0)
+			joinTable(commonFilter.getJoinList(), root);
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		query = predicates.length > 0 ? query.select(builder.count(root)).where(builder.and(predicates))
+				: query.select(builder.count(root));
+		Long count = getEntityManager().createQuery(query).getSingleResult();
+		return count;
+	}
+
+	/**
+	 * 数量
+	 *
+	 * @param nullFilter
+	 * @param eqFilters
+	 * @return
+	 */
+	public Long countDistinct(String field, CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Long> query = builder.createQuery(Long.class);
+		Root<T> root = query.from(clazz());
+		if (commonFilter.getJoinList().size() > 0)
+			joinTable(commonFilter.getJoinList(), root);
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		query = predicates.length > 0
+				? query.select(builder.countDistinct(root.get(field))).where(builder.and(predicates))
+				: query.select(builder.countDistinct(root.get(field)));
+		Long count = getEntityManager().createQuery(query).getSingleResult();
+		return count;
+	}
+
+	public Integer update(CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaUpdate<T> update = builder.createCriteriaUpdate(clazz());
+		Root<T> root = update.from(clazz());
+		commonFilter.getUpdatableList().stream().filter(x -> x.getValue() != null)
+				.forEach(x -> update.set(root.get(x.getField()), x.getValue()));
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		update.where(predicates);
+		int count = getEntityManager().createQuery(update).executeUpdate();
+		return count;
+	}
+
+	public Integer updateWithNull(CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaUpdate<T> update = builder.createCriteriaUpdate(clazz());
+		Root<T> root = update.from(clazz());
+		commonFilter.getUpdatableList().stream().forEach(x -> update.set(root.get(x.getField()), x.getValue()));
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		update.where(predicates);
+		int count = getEntityManager().createQuery(update).executeUpdate();
+		return count;
+	}
+
+	public int delete(CommonFilter commonFilter) {
+		CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+		CriteriaDelete<T> delete = builder.createCriteriaDelete(clazz());
+		Root<T> root = delete.from(clazz());
+		Predicate[] predicates = seperate(commonFilter, builder, root);
+		if (predicates.length == 0)
+			throw new JpaAmebaException("不支持的操作");
+		delete.where(builder.and(predicates));
+		int count = getEntityManager().createQuery(delete).executeUpdate();
+		return count;
+	}
+
+	protected Predicate[] seperate(CommonFilter commonFilter, CriteriaBuilder builder, Root<?> root) {
+		Predicate[] array = commonFilter.getList().stream().map(x -> createPredicate(x, builder, root))
+				.filter(x -> x != null).toArray(Predicate[]::new);
+		return array;
+	}
+
+	protected Path<?>[] groupBy(CommonFilter commonFilter, CriteriaBuilder builder, Root<?> root) {
+		Collection<GroupingElement<?>> grouping = commonFilter.getGroupingBy();
+		Path<?>[] array = grouping.stream().map(x -> root.get(x.getField())).toArray(Path[]::new);
+		return array;
+	}
+
+	private Predicate createPredicate(FilterElement<? extends Object> filter, CriteriaBuilder builder, Root<?> root) {
+		Path<?> path = root.get(filter.getField());
+		Object value = filter.getValue();
+		while (filter.getValue() instanceof FilterElement) {
+			filter = (FilterElement<?>) filter.getValue();
+			value = filter.getValue();
+			path = path.get(filter.getField());
+		}
+		if (value instanceof PathElement) {
+			Path<?> path2 = root;
+			for (String field : ((PathElement) value).getField().split(".")) {
+				path2 = path2.get(field);
+			}
+			value = path2;
+		}
+		if (value == null)
+			return null;
+		switch (filter.getFsy()) {
+		case EQ:
+			return builder.equal(path, value);
+		case NEQ:
+			return builder.notEqual(path, value);
+		case LIKE:
+			@SuppressWarnings("unchecked")
+			Path<String> path2 = (Path<String>) path;
+			return builder.like(path2, (String) value);
+		case ISNULL:
+			return builder.isNull(path);
+		case ISNOTNULL:
+			return builder.isNotNull(path);
+		case IN:
+			return path.in((Collection<?>) value);
+		case NOTIN:
+			return builder.not(path.in((Collection<?>) value));
+		default:
+			return createPredicate((ComparebleFilterElement<?>) filter, path.getParentPath(), builder);
+		}
+	}
+
+	private <Y extends Comparable<Y>> Predicate createPredicate(ComparebleFilterElement<Y> comparableFilter,
+			Path<?> path, CriteriaBuilder builder) {
+		String field = comparableFilter.getField();
+		Y value = comparableFilter.getValue();
+		switch (comparableFilter.getFsy()) {
+		case GE:
+			return builder.greaterThanOrEqualTo(path.get(comparableFilter.getField()), value);
+		case GT:
+			return builder.greaterThan(path.get(field), value);
+		case LE:
+			return builder.lessThanOrEqualTo(path.get(field), value);
+		case LT:
+			return builder.lessThan(path.get(field), value);
+		default:
+			break;
+		}
+		return null;
+	}
+
+	private List<Order> createOrderList(List<OrderBy> orderBies, Pageable page, CriteriaBuilder builder, Root<?> root) {
+		if (page.getOrderStr() != null) {
+			OrderBy orderBy = orderBies.stream().filter(x -> x.getField().equals(page.getOrderStr())).findFirst()
+					.orElse(null);
+			if (orderBy != null) {
+				orderBies.remove(orderBy);
+				orderBies.add(0, orderBy);
+			} else
+				orderBies.add(0, new OrderBy(page.getOrderStr(), page.getOrder()));
+		}
+		List<Order> orders = createOrderList(orderBies, builder, root);
+		return orders.stream().filter(x -> x != null).collect(toList());
+	}
+
+	private List<Order> createOrderList(List<OrderBy> orderBies, CriteriaBuilder builder, Root<?> root) {
+		List<Order> list = orderBies.stream().map(x -> createOrder(x.getField(), x.getValue(), builder, root))
+				.collect(toList());
+		return list;
+	}
+
+	private Order createOrder(String fields, OrderEnum order, CriteriaBuilder builder, Root<?> root) {
+		if (fields != null && order != null) {
+			List<String> fieldList = Arrays.asList(fields.split("\\.")).stream().filter(y -> StringUtils.isNotBlank(y))
+					.collect(toList());
+			String firstField = fieldList.remove(0);
+			Path<?> path = root.get(firstField);
+			for (String field : fieldList)
+				path = path.get(field);
+			return order == OrderEnum.ASC ? builder.asc(path) : builder.desc(path);
+		}
+		return null;
+	}
+
+	private void joinTable(List<JoinTable> list, Root<?> root) {
+		for (JoinTable joinTable : list)
+			root.join(joinTable.getField(), joinTable.getValue());
+	}
+
+	private List<Selection<?>> createSelectionList(Root<?> root, List<String> list) {
+		List<Selection<?>> selections = list.stream().map(x -> createSelection(root, x)).collect(toList());
+		return selections;
+	}
+
+	private Selection<?> createSelection(Root<?> root, String str) {
+		List<String> fields = Arrays.stream(str.split("\\.")).collect(toList());
+		Path<?> path = root.get(fields.remove(0));
+		for (String field : fields)
+			path = path.get(field);
+		return path;
+	}
+}
