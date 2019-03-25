b0e1b23fbfe17585e4bd2280bec624d862377b9a
donald <553430654@qq.com>
2019-03-25 15:34:32 +0800

    更新pom文件内容，spring更新到2.1.3，jar版本到0.1.4，一些依赖处理已增加额外的optional选项

--- src/main/java/com/kuding/config/AmebaConfig.java
+++ src/main/java/com/kuding/config/AmebaConfig.java
@@ -1,11 +1,14 @@
 package com.kuding.config;
 
+import org.hibernate.SessionFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.kuding.dao.CommonDao;
 
 @Configuration
+@ConditionalOnClass({ SessionFactory.class })
 public class AmebaConfig {
 
 	@Bean
